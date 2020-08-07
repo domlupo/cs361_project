d08d5533e6a4b714672e8ca596546648640e5fc8
Dominic Lupo <DominicLupo5318@gmail.com>
2020-08-07 04:09:11 -0400

    fix production bug with userLevelIDs on frontend.

--- src/util/util.js
+++ src/util/util.js
@@ -3,13 +3,22 @@ export const isDevelopment = () => {
 };
 
 export const isManagement = (user) => {
-  return user.userLevelID === 1 || user.userLevelID === 2;
+  return (
+    user.userLevelID === parseInt(process.env.REACT_APP_DB_OWNER_ID, 10) ||
+    user.userLevelID === parseInt(process.env.REACT_APP_DB_MANAGER_ID, 10)
+  );
 };
 
 export const isBuyer = (user) => {
-  return isManagement(user) || user.userLevelID === 3;
+  return (
+    isManagement(user) ||
+    user.userLevelID === parseInt(process.env.REACT_APP_DB_BUYER_ID, 10)
+  );
 };
 
 export const isCashier = (user) => {
-  return isManagement(user) || user.userLevelID === 4;
+  return (
+    isManagement(user) ||
+    user.userLevelID === parseInt(process.env.REACT_APP_DB_CASHIER_ID, 10)
+  );
 };
