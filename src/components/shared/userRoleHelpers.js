b0a9d6345673aa62373fd2322c593d7d60249cca
Dominic Lupo <DominicLupo5318@gmail.com>
2020-08-07 04:09:11 -0400

    fix production bug with userLevelIDs on frontend.

--- src/components/shared/userRoleHelpers.js
+++ src/components/shared/userRoleHelpers.js
@@ -1,60 +1,30 @@
 function getRoleFromID(id) {
-  if (process.env.NODE_ENV === 'development') {
-    switch (id) {
-      case 1:
-        return 'owner';
-      case 2:
-        return 'manager';
-      case 3:
-        return 'buyer';
-      case 4:
-        return 'cashier';
-      default:
-        return '';
-    }
-  } else {
-    switch (id) {
-      case 1:
-        return 'owner';
-      case 11:
-        return 'manager';
-      case 21:
-        return 'buyer';
-      case 31:
-        return 'cashier';
-      default:
-        return '';
-    }
+  switch (id) {
+    case process.env.REACT_APP_DB_OWNER_ID:
+      return 'owner';
+    case process.env.REACT_APP_DB_MANAGER_ID:
+      return 'manager';
+    case process.env.REACT_APP_DB_BUYER_ID:
+      return 'buyer';
+    case process.env.REACT_APP_DB_CASHIER_ID:
+      return 'cashier';
+    default:
+      return '';
   }
 }
 
 function getIDfromRole(role) {
-  if (process.env.NODE_ENV === 'development') {
-    switch (role.toLowerCase()) {
-      case 'owner':
-        return 1;
-      case 'manager':
-        return 2;
-      case 'buyer':
-        return 3;
-      case 'cashier':
-        return 4;
-      default:
-        return null;
-    }
-  } else {
-    switch (role.toLowerCase()) {
-      case 'owner':
-        return 1;
-      case 'manager':
-        return 11;
-      case 'buyer':
-        return 21;
-      case 'cashier':
-        return 31;
-      default:
-        return null;
-    }
+  switch (role.toLowerCase()) {
+    case 'owner':
+      return process.env.REACT_APP_DB_OWNER_ID;
+    case 'manager':
+      return process.env.REACT_APP_DB_MANAGER_ID;
+    case 'buyer':
+      return process.env.REACT_APP_DB_BUYER_ID;
+    case 'cashier':
+      return process.env.REACT_APP_DB_CASHIER_ID;
+    default:
+      return null;
   }
 }
 
--- src/util/util.js
+++ src/util/util.js
@@ -3,13 +3,18 @@ export const isDevelopment = () => {
 };
 
 export const isManagement = (user) => {
-  return user.userLevelID === 1 || user.userLevelID === 2;
+  return (
+    user.userLevelID === process.env.REACT_APP_DB_OWNER_ID ||
+    user.userLevelID === process.env.REACT_APP_DB_MANAGER_ID
+  );
 };
 
 export const isBuyer = (user) => {
-  return isManagement(user) || user.userLevelID === 3;
+  return (
+    isManagement(user) || user.userLevelID === process.env.REACT_APP_DB_BUYER_ID
+  );
 };
 
 export const isCashier = (user) => {
-  return isManagement(user) || user.userLevelID === 4;
+  return isManagement(user) || process.env.REACT_APP_DB_CASHIER_ID;
 };
