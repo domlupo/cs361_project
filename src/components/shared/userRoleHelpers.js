f22b80b6500cd972d826aaba4f1f1f634a5dd323
Dominic Lupo <dominiclupo5318@gmail.com>
2020-08-11 17:14:14 -0400

    fix production bug with userLevelIDs on frontend. (#64)

--- src/components/shared/userRoleHelpers.js
+++ src/components/shared/userRoleHelpers.js
@@ -1,60 +1,30 @@
 function getRoleFromID(id) {
-  if (process.env.NODE_ENV === 'development') {
-    switch (id) {
-      case 1:
-        return 'owner';
-      case 2:
-        return 'manager';
-      case 3:
-        return 'buyer';
-      case 4:
-        return 'cashier';
-      default:
-        return '';
-    }
-  } else {
-    switch (id) {
-      case 1:
-        return 'owner';
-      case 11:
-        return 'manager';
-      case 21:
-        return 'buyer';
-      case 31:
-        return 'cashier';
-      default:
-        return '';
-    }
+  switch (id) {
+    case parseInt(process.env.REACT_APP_DB_OWNER_ID, 10):
+      return 'owner';
+    case parseInt(process.env.REACT_APP_DB_MANAGER_ID, 10):
+      return 'manager';
+    case parseInt(process.env.REACT_APP_DB_BUYER_ID, 10):
+      return 'buyer';
+    case parseInt(process.env.REACT_APP_DB_CASHIER_ID, 10):
+      return 'cashier';
+    default:
+      return '';
   }
 }
 
 function getIDfromRole(role) {
-  if (process.env.NODE_ENV === 'development') {
-    switch (role.toLowerCase()) {
-      case 'owner':
-        return 1;
-      case 'manager':
-        return 2;
-      case 'buyer':
-        return 3;
-      case 'cashier':
-        return 4;
-      default:
-        return null;
-    }
-  } else {
-    switch (role.toLowerCase()) {
-      case 'owner':
-        return 1;
-      case 'manager':
-        return 11;
-      case 'buyer':
-        return 21;
-      case 'cashier':
-        return 31;
-      default:
-        return null;
-    }
+  switch (role.toLowerCase()) {
+    case 'owner':
+      return parseInt(process.env.REACT_APP_DB_OWNER_ID, 10);
+    case 'manager':
+      return parseInt(process.env.REACT_APP_DB_MANAGER_ID, 10);
+    case 'buyer':
+      return parseInt(process.env.REACT_APP_DB_BUYER_ID, 10);
+    case 'cashier':
+      return parseInt(process.env.REACT_APP_DB_CASHIER_ID, 10);
+    default:
+      return null;
   }
 }
 
