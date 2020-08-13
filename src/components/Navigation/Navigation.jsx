87d48b366d35d3c3b1078dd8d02190f1a35e0a75
Dominic Lupo <dominiclupo5318@gmail.com>
2020-08-13 14:54:52 -0400

    Remove cashier and buyer from seing 'add user' and 'add product' form. (#67)

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { NavLink, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/logo/logo.svg';
-
+import { isManagement } from '../../util/util';
 import './Navigation.css';
 
 export default function Navigation({ children }) {
+  const currentUser = useSelector((state) => state.user);
+
   return (
     <main className="MainNavigation">
       <section className="NavigationSide">
@@ -26,13 +29,15 @@ export default function Navigation({ children }) {
         >
           Products
         </NavLink>
-        <NavLink
-          className="NavigationLink"
-          to="/product-insert"
-          activeClassName="NavigationLinkActive"
-        >
-          Add New Product Type
-        </NavLink>
+        {isManagement(currentUser) && (
+          <NavLink
+            className="NavigationLink"
+            to="/product-insert"
+            activeClassName="NavigationLinkActive"
+          >
+            Add New Product Type
+          </NavLink>
+        )}
         <NavLink
           className="NavigationLink"
           to="/transactions"
@@ -47,13 +52,15 @@ export default function Navigation({ children }) {
         >
           Users
         </NavLink>
-        <NavLink
-          className="NavigationLink"
-          to="/user-insert"
-          activeClassName="NavigationLinkActive"
-        >
-          Add New User
-        </NavLink>
+        {isManagement(currentUser) && (
+          <NavLink
+            className="NavigationLink"
+            to="/user-insert"
+            activeClassName="NavigationLinkActive"
+          >
+            Add New User
+          </NavLink>
+        )}
         <hr />
       </section>
       <div className="NavigationSideContainer" />
